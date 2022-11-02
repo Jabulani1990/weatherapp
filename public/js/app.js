ae2e9c81b76ac81bdaa9fe4967fefb7e68365213
Oyewale <olawaleabdulganiyujabulani@gmail.com>
2022-11-02 12:23:43 +0100

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 
 
-fetch('http://localhost:3000/weather?lat=144.34&lon=110.99').then((response)=>{
+fetch('/weather?lat=144.34&lon=110.99').then((response)=>{
         response.json().then((data)=>{
             if(data.error){
                 console.log(data.error)
